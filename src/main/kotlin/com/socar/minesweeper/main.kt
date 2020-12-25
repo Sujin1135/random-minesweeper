a7b6a9733831eca552bba2597e09d21cf9348a82
iam-mz-mkchoi-test-at-179248873946 <mzdev>
2020-12-25 23:53:47 +0900

    :fire: remove printing mine locations

--- src/main/kotlin/com/socar/minesweeper/main.kt
+++ src/main/kotlin/com/socar/minesweeper/main.kt
@@ -5,13 +5,6 @@ import com.socar.minesweeper.domain.Field
 fun main(args: Array<String>) {
     val field = Field()
 
-    // print mines
-    for (i in field.mineLocations) {
-        print("$i, ")
-    }
-
-    println("-----")
-
     // print cells of the minesweeper game
     for (arr in field.cells) {
         for (i in arr) {
